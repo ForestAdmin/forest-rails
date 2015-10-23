14439d6b0c93b2c8ff6958eca5b58049318ad9eb
Sandro Munda <sandro@munda.me>
2015-10-23 12:13:14 +0200

    Fix the JSONAPI Mime type warning

--- config/initializers/mimetype.rb
+++ config/initializers/mimetype.rb
@@ -1,1 +1,14 @@
-Mime::Type.register "application/json", :json, %w( text/x-json application/jsonrequest application/vnd.api+json )
+api_mime_types = %W(
+  application/vnd.api+json
+  text/x-json
+  application/json
+)
+
+api_mime_types = %w(
+  text/x-json
+  application/jsonrequest
+  application/vnd.api+json
+)
+
+Mime::Type.unregister :json
+Mime::Type.register 'application/json', :json, api_mime_types
