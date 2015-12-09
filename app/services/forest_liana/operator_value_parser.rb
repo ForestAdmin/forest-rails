578eac80228e6c4b2c49307de0ade355ebe8cdd4
Sandro Munda <sandro@munda.me>
2015-12-09 12:16:12 +0100

    Support relative datetime

--- app/services/forest_liana/operator_value_parser.rb
+++ app/services/forest_liana/operator_value_parser.rb
@@ -26,6 +26,21 @@ module ForestLiana
         operator = '='
       end
 
+      case value
+      when 'yesterday'
+        value = 1.day.ago
+      when 'lastWeek'
+        value = 1.week.ago
+      when 'last2Weeks'
+        value = 2.week.ago
+      when 'lastMonth'
+        value = 1.month.ago
+      when 'last3Months'
+        value = 3.month.ago
+      when 'lastYear'
+        value = 1.year.ago
+      end
+
       [operator, value]
     end
 
