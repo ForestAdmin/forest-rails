d38489e1d8faaeebe5ef12edc8d37ee60596a9c3
Sandro Munda <sandro@munda.me>
2016-03-23 10:14:37 +0100

    Change the way to create a smart collection

--- app/services/forest_liana/collection.rb
+++ app/services/forest_liana/collection.rb
@@ -1,37 +1,7 @@
 class ForestLiana::Collection
-  mattr_accessor :collection_name
-  mattr_accessor :is_read_only
 
-  def self.field(field, opts)
-    collection = ForestLiana.apimap.find do |x|
-      x.name ==  self.collection_name.try(:to_s)
-    end
-
-    collection.fields << opts.merge({ field: field }) if collection
-  end
-
-  def self.fields(fields)
-    collection = ForestLiana::Model::Collection.new({
-      name: self.collection_name,
-      is_read_only: self.is_read_only,
-      fields: fields
-    })
-
-    ForestLiana.apimap << collection
-  end
-
-  def self.action(name, opts = {})
-    collection = ForestLiana.apimap.find do |x|
-      x.name == self.collection_name.try(:to_s)
-    end
-
-    return if collection.blank?
-
-    collection.actions << ForestLiana::Model::Action.new({
-      name: name,
-      http_method: opts[:http_method],
-      endpoint: opts[:endpoint],
-      fields: opts[:fields]
-    })
+  def self.add_actions(collection_name, actions)
+    collection = ForestLiana.apimap.find {|x| x.name == collection_name}
+    collection.actions += actions if collection
   end
 end
