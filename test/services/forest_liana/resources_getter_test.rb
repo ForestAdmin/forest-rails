3254f87c656bb108d37864964300faecd137460c
Sandro Munda <sandro@munda.me>
2016-04-04 09:49:14 +0200

    Remove hasmany old tests

--- test/services/forest_liana/resources_getter_test.rb
+++ test/services/forest_liana/resources_getter_test.rb
@@ -74,35 +74,5 @@ module ForestLiana
       assert records.first.belongs_to_field.id == 30
       assert records.last.belongs_to_field.id == 21
     end
-
-    #test 'Sort by a has_many association' do
-      #getter = ResourcesGetter.new(HasManyField, {
-        #page: { size: 10, number: 1 },
-        #sort: '-belongs_to_fields'
-      #})
-      #getter.perform
-      #records = getter.records
-      #count = getter.count
-
-      #assert records.count == 10
-      #assert count = 30
-      #assert records.first.id = 7
-    #end
-
-    #test 'Sort by a has_many through association' do
-      #getter = ResourcesGetter.new(HasManyThroughField, {
-        #page: { size: 10, number: 1 },
-        #sort: '-belongs_to_fields'
-      #})
-      #getter.perform
-      #records = getter.records
-      #count = getter.count
-
-      #assert records.count == 10
-      #assert count = 30
-      #assert records.first.id = 2
-      #assert records.second.id = 3
-    #end
-
   end
 end
