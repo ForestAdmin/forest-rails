ba42467b8a77ed26bbbf572ec2951519f29ea15b
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-06-09 12:09:20 +0200

    [*] Error Messages - Display an explicit warning if Forest servers are in maintenance

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -12,7 +12,8 @@ module ForestLiana
         }
 
         logger.formatter = proc do |severity, datetime, progname, message|
-          displayed_message = "[#{datetime.to_s(:db)}] Forest 🌳🌳🌳  #{message}\n"
+          displayed_message = "[#{datetime.to_s(:db)}] Forest 🌳🌳🌳  " \
+            "#{message}\n"
           "\e[#{logger_colors[severity.to_sym]}m#{displayed_message}\033[0m"
         end
 
