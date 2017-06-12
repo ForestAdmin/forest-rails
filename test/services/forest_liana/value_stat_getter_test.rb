c06380347b515e936bf813f5ebdf6c7cf91b409a
Lucas Scariot <lucas@forestadmin.com>
2017-06-12 15:27:16 +0200

    [+] Add test to the ressourceGetter

---
+++ test/services/forest_liana/value_stat_getter_test.rb
@@ -0,0 +1,23 @@
+module ForestLiana
+  class ValueStatGetterTest < ActiveSupport::TestCase
+
+
+    test 'Value stat getter' do
+      stat = ValueStatGetter.new(BelongsToField, {
+        type: "Value",
+        collection: "belongs_to_field",
+        timezone: "+02:00",
+        aggregate: "Count",
+        filterType: "and",
+        filters: [{
+          field: "has_one_field:id",
+          value: "3"
+        }]
+      })
+
+      stat.perform
+
+      assert stat.record.value[:countCurrent] == 1
+    end
+  end
+end
