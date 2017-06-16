0476ac31026430d76aae014536724f94fb1f406b
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-06-16 09:16:25 +0200

    Remove tests

--- test/dummy/app/models/serialize_and_belongs_to_field.rb
+++
@@ -1,5 +0,0 @@
-class SerializeAndBelongsToField < ActiveRecord::Base
-  serialize :field, Array
-
-  belongs_to :has_one_field
-end
--- test/dummy/db/migrate/20170615120421_create_serialize_and_belongs_to_field.rb
+++
@@ -1,8 +0,0 @@
-class CreateSerializeAndBelongsToField < ActiveRecord::Migration
-  def change
-    create_table :serialize_and_belongs_to_fields do |t|
-      t.string :field
-      t.references :has_one_field, index: true
-    end
-  end
-end
