dcae9f5a8751dbbfc96345816e6ede3c9e66128d
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-06-22 19:31:33 +0200

    Fix the tests

--- test/services/forest_liana/resource_updater_test.rb
+++ test/services/forest_liana/resource_updater_test.rb
@@ -25,6 +25,25 @@ module ForestLiana
       updater.perform
 
       assert updater.record.valid?
+      assert updater.record.field == "value 1"
+    end
+
+    test 'Update a record on a "serialize" attribute with a null value' do
+      params = ActionController::Parameters.new(
+        id: 1,
+        data: {
+          id: 1,
+          type: "serialize_field",
+          attributes: {
+            field: nil
+          }
+        }
+      )
+      updater = ResourceUpdater.new(SerializeField, params)
+      updater.perform
+
+      assert updater.record.valid?
+      byebug
       assert updater.record.field == []
     end
 
