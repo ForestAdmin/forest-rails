1080e63c61010804ae8a02fdc919190e724e4793
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-07-13 16:23:02 +0200

    [*] Pie Charts - Fix potential pie chart rendering issue using Rails 5 (#169)

---
+++ test/services/forest_liana/pie_stat_getter_test.rb
@@ -0,0 +1,29 @@
+module ForestLiana
+  class PieStatGetterTest < ActiveSupport::TestCase
+    test 'Pie stat getter with an aggregate on a boolean field' do
+      stat = PieStatGetter.new(BooleanField, {
+        type: "Pie",
+        collection: "boolean_field",
+        timezone: "+02:00",
+        aggregate: "Count",
+        group_by_field: "field"
+      })
+
+      stat.perform
+      assert stat.record.value.count == 0
+    end
+
+    test 'Pie stat getter with an aggregate on a foreign key' do
+      stat = PieStatGetter.new(BelongsToField, {
+        type: "Pie",
+        collection: "belongs_to_field",
+        timezone: "+02:00",
+        aggregate: "Count",
+        group_by_field: "has_one_field_id"
+      })
+
+      stat.perform
+      assert stat.record.value.count == 30
+    end
+  end
+end
