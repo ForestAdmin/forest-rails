391381a0445885b7621da2fcdfe021895cac04b2
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-10-16 18:56:46 +0200

    Add some tests

--- test/dummy/db/migrate/20150608133044_create_has_one_field.rb
+++ test/dummy/db/migrate/20150608133044_create_has_one_field.rb
@@ -1,6 +1,8 @@
 class CreateHasOneField < ActiveRecord::Migration
   def change
     create_table :has_one_fields do |t|
+      t.boolean :checked
+      t.column :status, :integer, default: 0
     end
   end
 end
