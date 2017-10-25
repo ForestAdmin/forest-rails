f23619ec437ad1e3f8975a47996d49ce61215db0
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-10-25 14:05:23 +0200

    Clean commented code

--- app/services/forest_liana/intercom_conversations_getter.rb
+++ app/services/forest_liana/intercom_conversations_getter.rb
@@ -7,7 +7,6 @@ module ForestLiana
     end
 
     def count
-      #return 1
       @records.count
     end
 
@@ -34,52 +33,6 @@ module ForestLiana
       rescue Intercom::UnexpectedError => exception
         FOREST_LOGGER.error "Cannot retrieve the Intercom conversations: #{exception.message}"
         @records = []
-#         conv = ::Intercom::Conversation.new({
-#   "type": "conversation",
-#   "id": "147",
-#   "created_at": 1400850973,
-#   "updated_at": 1400857494,
-#   "waiting_since": 1400857494,
-#   "snoozed_until": nil,
-#   conversation_message: ::Intercom::Message.new({
-#     "type": "conversation_message",
-#     "subject": "tot",
-#     "body": "<p>Hi Alice,</p>\n\n<p> We noticed you using our Product,  do you have any questions?</p> \n<p>- Jane</p>",
-#     "author": {
-#       "type": "admin",
-#       "id": "25"
-#     },
-#     "attachments": [
-#       {
-#         "name": "signature",
-#         "url": "http://example.org/signature.jpg"
-#       }
-#     ]
-#   }),
-#   "user": {
-#     "type": "user",
-#     "id": "536e564f316c83104c000020"
-#   },
-#   "customers": [
-#     {
-#       "type": "user",
-#       "id": "58ff3f670f14ab4f1aa83750"
-#     }
-#   ],
-#   "assignee": {
-#     "type": "admin",
-#     "id": "25"
-#   },
-#   "open": true,
-#   "state": "open",
-#   "read": true,
-#   "conversation_parts": {
-#     "type": "conversation_part.list",
-#     "total_count":1
-#   },
-#   "tags": { "type": 'tag.list', "tags": [] }
-# })
-#         @records = [conv]
       end
     end
 
