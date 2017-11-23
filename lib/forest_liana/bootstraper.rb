b7cb9770541f752f0c675763740c38c973e87fb6
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-11-23 14:45:56 +0100

    [*] Collection name - Fix name for models in namespaces

--- lib/forest_liana/bootstraper.rb
+++ lib/forest_liana/bootstraper.rb
@@ -121,8 +121,7 @@ module ForestLiana
         .try(:each) do |table_name, models|
           models.each do |model|
             unless model.name.deconstantize.blank?
-              ForestLiana.names_overriden[model] =
-                "#{model.name.deconstantize.downcase}__#{model.name}"
+              ForestLiana.names_overriden[model] = model.name.gsub('::', '__')
               # TODO: Remove once lianas prior to 2.0.0 are not supported.
               ForestLiana.names_old_overriden[model] =
                 "#{model.name.deconstantize.downcase}__#{model.table_name}"
