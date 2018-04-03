71d5b2f1e8692fa8b737a85fa9f14cceac961349
Arnaud Besnier <arnaudibesnier@gmail.com>
2018-04-03 14:30:10 +0200

    [+] Related Data - Delete records directly from a hasMany listing

--- app/services/forest_liana/has_many_dissociator.rb
+++ app/services/forest_liana/has_many_dissociator.rb
@@ -4,6 +4,7 @@ module ForestLiana
       @resource = resource
       @association = association
       @params = params
+      @with_deletion = @params[:delete].to_s == 'true'
       @data = params['data']
     end
 
@@ -11,10 +12,26 @@ module ForestLiana
       @record = @resource.find(@params[:id])
       associated_records = @resource.find(@params[:id]).send(@association.name)
 
-      if @data.is_a?(Array)
-        @data.each do |record_deleted|
-          associated_records.delete(
-            @association.klass.find(record_deleted[:id]))
+      remove_association = false
+
+      if @with_deletion
+        remove_association = (@association.macro == :has_and_belongs_to_many)
+      else
+        remove_association = true
+      end
+
+      if remove_association
+        if @data.is_a?(Array)
+          @data.each do |record_deleted|
+            associated_records.delete(@association.klass.find(record_deleted[:id]))
+          end
+        end
+      end
+
+      if @with_deletion
+        if @data.is_a?(Array)
+          record_ids = @data.map { |record| record[:id] }
+          @association.klass.destroy(record_ids)
         end
       end
     end
