5ed3a122aeb0589df169b305931c1691f06ad743
Arnaud Besnier <arnaudibesnier@gmail.com>
2018-06-04 09:13:39 +0200

    [-] Code Style - Improvements

--- app/controllers/forest_liana/resources_controller.rb
+++ app/controllers/forest_liana/resources_controller.rb
@@ -15,13 +15,12 @@ module ForestLiana
 
     def index
       begin
-
         if request.format == 'csv'
-          return head :forbidden if !PermissionsChecker.new(@resource, "export").is_authorized
+          return head :forbidden if !PermissionsChecker.new(@resource, 'export').is_authorized
         elsif params.has_key?(:searchToEdit)
-          return head :forbidden if !PermissionsChecker.new(@resource, "searchToEdit").is_authorized
+          return head :forbidden if !PermissionsChecker.new(@resource, 'searchToEdit').is_authorized
         else
-          return head :forbidden if !PermissionsChecker.new(@resource, "list").is_authorized
+          return head :forbidden if !PermissionsChecker.new(@resource, 'list').is_authorized
         end
 
         getter = ForestLiana::ResourcesGetter.new(@resource, params)
@@ -39,7 +38,7 @@ module ForestLiana
 
     def show
       begin
-        return head :forbidden if !PermissionsChecker.new(@resource, "show").is_authorized
+        return head :forbidden if !PermissionsChecker.new(@resource, 'show').is_authorized
 
         getter = ForestLiana::ResourceGetter.new(@resource, params)
         getter.perform
@@ -54,7 +53,7 @@ module ForestLiana
 
     def create
       begin
-        return head :forbidden if !PermissionsChecker.new(@resource, "create").is_authorized
+        return head :forbidden if !PermissionsChecker.new(@resource, 'create').is_authorized
 
         creator = ForestLiana::ResourceCreator.new(@resource, params)
         creator.perform
@@ -77,7 +76,7 @@ module ForestLiana
 
     def update
       begin
-        return head :forbidden if !PermissionsChecker.new(@resource, "update").is_authorized
+        return head :forbidden if !PermissionsChecker.new(@resource, 'update').is_authorized
 
         updater = ForestLiana::ResourceUpdater.new(@resource, params)
         updater.perform
@@ -100,7 +99,7 @@ module ForestLiana
 
     def destroy
       begin
-        return head :forbidden if !PermissionsChecker.new(@resource, "delete").is_authorized
+        return head :forbidden if !PermissionsChecker.new(@resource, 'delete').is_authorized
 
         @resource.destroy(params[:id])
         head :no_content
