7d33bac076a095b653a7881d722333d692bbb778
Arnaud Besnier <arnaudibesnier@gmail.com>
2018-06-20 11:00:41 +0200

    [*] Onboarding - The install generator now supports credentials.yml.enc file introduced in Rails 5.2

--- lib/generators/forest_liana/install_generator.rb
+++ lib/generators/forest_liana/install_generator.rb
@@ -14,30 +14,40 @@ module ForestLiana
 
       route "mount ForestLiana::Engine => '/forest'"
 
-      initializer 'forest_liana.rb' do
-        "ForestLiana.env_secret = Rails.application.secrets.forest_env_secret" +
-        "\nForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret"
-      end
+      byebug
+
+      # NOTICE: Detect Rails 5.2+ apps for the new onboarding behaviour.
+      might_use_encrypted_credentials = Rails::VERSION::MAJOR > 4 && Rails::VERSION::MINOR > 1
+      file_secrets_exists = File.exist? 'config/secrets.yml'
 
       auth_secret = SecureRandom.urlsafe_base64
 
-      puts "\nForest generated a random authentication secret to secure the " +
-        "data access of your local project.\nYou can change it at any time in " +
-        "your config/secrets.yml file.\n\n"
+      if file_secrets_exists
+        initializer 'forest_liana.rb' do
+          "ForestLiana.env_secret = Rails.application.secrets.forest_env_secret" +
+          "\nForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret"
+        end
 
-      inject_into_file 'config/secrets.yml', after: "development:\n" do
-        "  forest_env_secret: #{env_secret}\n" +
-        "  forest_auth_secret: #{auth_secret}\n"
-      end
+        puts "\nForest generated a random authentication secret to secure the " +
+          "data access of your local project.\nYou can change it at any time in " +
+          "your config/secrets.yml file.\n\n"
 
-      inject_into_file 'config/secrets.yml', after: "staging:\n", force: true do
-        "  forest_env_secret: <%= ENV[\"FOREST_ENV_SECRET\"] %>\n" +
-        "  forest_auth_secret: <%= ENV[\"FOREST_AUTH_SECRET\"] %>\n"
-      end
+        inject_into_file 'config/secrets.yml', after: "development:\n" do
+          "  forest_env_secret: #{env_secret}\n" +
+          "  forest_auth_secret: #{auth_secret}\n"
+        end
 
-      inject_into_file 'config/secrets.yml', after: "production:\n", force: true do
-        "  forest_env_secret: <%= ENV[\"FOREST_ENV_SECRET\"] %>\n" +
-        "  forest_auth_secret: <%= ENV[\"FOREST_AUTH_SECRET\"] %>\n"
+        inject_into_file 'config/secrets.yml', after: "staging:\n", force: true do
+          "  forest_env_secret: <%= ENV[\"FOREST_ENV_SECRET\"] %>\n" +
+          "  forest_auth_secret: <%= ENV[\"FOREST_AUTH_SECRET\"] %>\n"
+        end
+
+        inject_into_file 'config/secrets.yml', after: "production:\n", force: true do
+          "  forest_env_secret: <%= ENV[\"FOREST_ENV_SECRET\"] %>\n" +
+          "  forest_auth_secret: <%= ENV[\"FOREST_AUTH_SECRET\"] %>\n"
+        end
+      else
+        # TODO: Implement the onboarding with Rails 5.2 apps.
       end
     end
   end
