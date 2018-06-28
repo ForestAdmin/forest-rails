f65a6e7d4ef2952ac6364c966f80be49bf3c148f
Arnaud Besnier <arnaudibesnier@gmail.com>
2018-06-28 13:28:16 +0200

    Simplify the code

--- app/utils/records-decorator.rb
+++ app/utils/records-decorator.rb
@@ -1,24 +1,16 @@
 def decorate_for_search(records_serialized, field_names, search_value)
-  matchFields = {}
+  match_fields = {}
   records_serialized['data'].each_with_index do |record, index|
     field_names.each do |field_name|
       value = record['attributes'][field_name]
       if value
         match = value.match(/#{search_value}/i)
         if match
-          if matchFields[index].nil?
-            matchFields[index] = {
-              id: record['id'],
-              search: []
-            }
-          end
-          matchFields[index][:search] << field_name
+          match_fields[index] = { id: record['id'], search: [] } if match_fields[index].nil?
+          match_fields[index][:search] << field_name
         end
       end
     end
   end
-  if matchFields.empty?
-    matchFields = nil
-  end
-  return matchFields
+  match_fields.empty? ? nil : match_fields
 end
