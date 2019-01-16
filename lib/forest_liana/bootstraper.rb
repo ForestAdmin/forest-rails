080cad807a0d8f802ba6e4ae9eab27a68ea03147
Vincent Molinié <vincent.molinie@epita.fr>
2019-01-16 17:31:03 +0100

    Adding pretty print file

--- lib/forest_liana/bootstraper.rb
+++ lib/forest_liana/bootstraper.rb
@@ -211,6 +211,60 @@ module ForestLiana
       end
     end
 
+    def pretty_print_json(json, indentation = "")
+      result = ""
+
+      if json.kind_of?(Array)
+        result << "["
+        is_small = json.length < 3
+        is_primary_value = false
+        json.each_index do |index|
+          item = json[index]
+          is_primary_value = !item.kind_of?(Hash) && !item.kind_of?(Array)
+
+          if index == 0 && is_primary_value && !is_small
+            result << "\n" << indentation << "  "
+          elsif index > 0 && is_primary_value && !is_small
+            result << ",\n" << indentation << "  "
+          elsif index > 0
+            result << ", "
+          end
+
+          result << pretty_print_json(item, is_primary_value ? indentation + "  " : indentation);
+        end
+
+        if is_primary_value && !is_small
+          result << "\n" << indentation
+        end
+        result << "]"
+      elsif json.kind_of?(Hash)
+        result << "{\n"
+
+        is_first = true
+        json = json.stringify_keys
+        json.each do |key, value|
+          unless is_first
+            result << ",\n"
+          end
+          is_first = false
+          result << indentation << '  "' << key << '": '
+          result << pretty_print_json(value, indentation + "  ")
+        end
+
+        result << "\n" << indentation << "}"
+      elsif json.nil?
+        result << "null"
+      elsif !!json == json
+        result << (json ? "true" : "false")
+      elsif json.is_a? String
+        result << '"' << json << '"'
+      else
+        result << json.to_s
+      end
+
+      result
+    end
+
     def update_schema_file
       File.open(File.join(Rails.root, 'forestadmin-schema.json'), 'w') do |f|
         collections = ForestLiana.apimap.as_json
@@ -306,7 +360,7 @@ module ForestLiana
         end
         collections.sort { |collection1, collection2| collection1['name'] <=> collection2['name'] }
 
-        f.puts JSON.pretty_generate({
+        f.puts pretty_print_json({
           collections: collections,
           meta: ForestLiana.meta
         })
