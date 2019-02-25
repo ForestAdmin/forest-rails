2941f286d6dba05a92d65df6537667ac3533c1f1
Arnaud Besnier <arnaudibesnier@gmail.com>
2019-02-25 12:06:17 +0100

    [-] Tests - Add more tests

--- spec/query_helper_spec.rb
+++ spec/query_helper_spec.rb
@@ -2,14 +2,24 @@ module ForestLiana
   describe QueryHelper do
     before(:all) do
       Tree.connection
+      Owner.connection
     end
 
     describe 'get_one_associations' do
-      it 'should return the one-one associations' do
-        associations = QueryHelper.get_one_associations(Tree)
-        expect(associations.length).to eq(1)
-        expect(associations.first.name).to eq(:owner)
-        expect(associations.first.klass).to eq(Owner)
+      context 'on a model having a hasMany association' do
+        it 'should not return any one-one associations' do
+          associations = QueryHelper.get_one_associations(Owner)
+          expect(associations.length).to eq(0)
+        end
+      end
+
+      context 'on a model having a belongsTo association' do
+        it 'should return the one-one associations' do
+          associations = QueryHelper.get_one_associations(Tree)
+          expect(associations.length).to eq(1)
+          expect(associations.first.name).to eq(:owner)
+          expect(associations.first.klass).to eq(Owner)
+        end
       end
     end
 
