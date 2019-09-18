8e19b2ceedcb68e1b5faf0c1ca45069ac83e7f0f
Rob Whittaker <rob@purinkle.co.uk>
2019-09-18 16:41:31 +0100

    [+] Configurations - Users can specify the directory for Forest Smart Implementation (#327)

--- test/forest_liana_test.rb
+++ test/forest_liana_test.rb
@@ -4,4 +4,22 @@ class ForestLianaTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, ForestLiana
   end
+
+  test 'config_dirs with no value set' do
+    assert_equal(
+      Rails.root.join('lib/forest_liana/**/*.rb'),
+      ForestLiana.config_dir
+    )
+  end
+
+  test 'config_dirs with a value set' do
+    ForestLiana.config_dir = 'lib/custom/**/*.rb'
+
+    assert_equal(
+      Rails.root.join('lib/custom/**/*.rb'),
+      ForestLiana.config_dir
+    )
+
+    ForestLiana.config_dir = nil
+  end
 end
