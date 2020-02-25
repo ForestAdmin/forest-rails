7a5cbe75c7be182b6d336b1f872822f149c3b38c
Raphaël Huchet <raphaelht@gmail.com>
2020-02-25 16:57:15 +0100

    has many dissociation

--- app/services/forest_liana/has_many_dissociator.rb
+++ app/services/forest_liana/has_many_dissociator.rb
@@ -14,20 +14,31 @@ module ForestLiana
 
       remove_association = !@with_deletion || @association.macro == :has_and_belongs_to_many
 
+      record_ids = Array.new
+
       if remove_association
-        if @data.is_a?(Array)
-          @data.each do |record_deleted|
-            associated_records.delete(@association.klass.find(record_deleted[:id]))
+        if @data.is_a?(Array) && @data.dig('attributes') == nil
+          record_ids = @data.map { |record| record[:id] }
+        elsif @data.dig('attributes') != nil
+          record_ids = ForestLiana::ResourcesGetter.get_ids_from_request(@params)
+        end
+        if record_ids.any?
+          record_ids.each do |id|
+            associated_records.delete(@association.klass.find(id))
           end
         end
       end
 
       if @with_deletion
-        if @data.is_a?(Array)
+        if @data.is_a?(Array) && @data.dig('attributes') == nil
           record_ids = @data.map { |record| record[:id] }
+        elsif @data.dig('attributes') != nil
+          record_ids = ForestLiana::ResourcesGetter.get_ids_from_request(@params)
+        end
+        if record_ids.any?
           record_ids = record_ids.select { |record_id| @association.klass.exists?(record_id) }
           @association.klass.destroy(record_ids)
-        end
+        end 
       end
     end
   end
