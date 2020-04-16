c69d29924b76ce979a01c9067401b3b05a705830
DrRaider <nsailly@live.fr>
2020-04-16 15:08:33 +0200

    feat(smart-action): allow users to protect their smart-action APIs from unauthorized usage

--- app/controllers/forest_liana/application_controller.rb
+++ app/controllers/forest_liana/application_controller.rb
@@ -16,9 +16,11 @@ module ForestLiana
     if Rails::VERSION::MAJOR < 4
       before_filter :authenticate_user_from_jwt
       before_filter :set_paper_trail_whodunnit if self.papertrail?
+      before_filter :check_permission_for_smart_route
     else
       before_action :authenticate_user_from_jwt
       before_action :set_paper_trail_whodunnit if self.papertrail?
+      before_action :check_permission_for_smart_route
     end
 
     if self.papertrail?
@@ -106,6 +108,42 @@ module ForestLiana
     end
 
     private
+
+    def check_permission_for_smart_route
+      begin
+        if request.post?
+          body = ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+          if body.has_key?(:smart_action_id)
+            checker = ForestLiana::PermissionsChecker.new(
+              find_resource(body[:collection_name]),
+              'actions', @rendering_id,
+              get_smart_action_info_from_request(forest_user, body)
+            )
+            return head :forbidden unless checker.is_authorized?
+          else
+            render serializer: nil, json: { status: 403 }, status: :bad_request
+          end
+        end
+      rescue => error
+        FOREST_LOGGER.error "Smart Action execution error: #{error}"
+        render serializer: nil, json: { status: 403 }, status: :bad_request
+      end
+    end
+
+    def find_resource(collection_name)
+      begin
+          resource = SchemaUtils.find_model_from_collection_name(collection_name)
+
+          if resource.nil? || !SchemaUtils.model_included?(resource) ||
+            !resource.ancestors.include?(ActiveRecord::Base)
+            render serializer: nil, json: { status: 404 }, status: :not_found
+          end
+          resource
+      rescue => error
+        FOREST_LOGGER.error "Find Collection error: #{error}\n#{format_stacktrace(error)}"
+        render serializer: nil, json: { status: 404 }, status: :not_found
+      end
+    end
 
     def force_utf8_encoding(json)
       if json['data'].class == Array
@@ -237,6 +275,13 @@ module ForestLiana
 
     def format_stacktrace error
       error.backtrace.join("\n\t")
+    end
+
+    def get_smart_action_info_from_request(user, body) 
+      {
+        "userId" => user['id'],
+        "actionId" => body[:smart_action_id],
+      }
     end
   end
 end
