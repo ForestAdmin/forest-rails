6359c8d3ee862d515617712d5a25368f25f961e8
Valentin Lamatte <valentinl@forestadmin.com>
2020-11-24 15:12:25 +0100

    test(permissions): add error test

--- spec/services/forest_liana/permissions_getter_spec.rb
+++ spec/services/forest_liana/permissions_getter_spec.rb
@@ -10,8 +10,6 @@ module ForestLiana
       let(:rendering_id) { 34 }
       let(:liana_permissions_url) { 'https://api.forestadmin.com/liana/v3/permissions' }
       let(:liana_permissions_api_call_response) { instance_double(HTTParty::Response) }
-      let(:liana_permissions_api_call_response_content) { Net::HTTPOK.new({}, 200, liana_permissions_api_call_response_content_body) }
-      let(:liana_permissions_api_call_response_content_body) { '{"test": true}' }
       let(:expected_query_parameters) {
         {
           :headers => {
@@ -26,17 +24,41 @@ module ForestLiana
         allow(HTTParty).to receive(:get).and_return(liana_permissions_api_call_response)
         allow(liana_permissions_api_call_response).to receive(:response).and_return(liana_permissions_api_call_response_content)
         allow(liana_permissions_api_call_response_content).to receive(:body).and_return(liana_permissions_api_call_response_content_body)
-        allow(JSON).to receive(:parse)
-
-        described_class.get_permissions_for_rendering(rendering_id)
       end
 
-      it 'should call the API with correct URL' do
-        expect(HTTParty).to have_received(:get).with(liana_permissions_url, expected_query_parameters)
+      describe 'when the API returns a success' do
+        let(:liana_permissions_api_call_response_content) { Net::HTTPOK.new({}, 200, liana_permissions_api_call_response_content_body) }
+        let(:liana_permissions_api_call_response_content_body) { '{"test": true}' }
+        let(:expected_parsed_result) { { "test" => true } }
+
+        it 'should call the API with correct URL' do
+          described_class.get_permissions_for_rendering(rendering_id)
+          expect(HTTParty).to have_received(:get).with(liana_permissions_url, expected_query_parameters)
+        end
+
+        it 'should return the expected JSON body' do
+          expect(described_class.get_permissions_for_rendering(rendering_id)).to eq expected_parsed_result
+        end
       end
 
-      it 'should return the expected JSON body' do
-        expect(JSON).to have_received(:parse).with(liana_permissions_api_call_response_content_body)
+      describe 'when the API returns a not found error' do
+        let(:liana_permissions_api_call_response_content) { Net::HTTPNotFound.new({}, 404, liana_permissions_api_call_response_content_body) }
+        let(:liana_permissions_api_call_response_content_body) { 'Not Found' }
+
+        before do
+          allow(FOREST_LOGGER).to receive(:error)
+        end
+
+        it 'should return nil' do
+          expect(described_class.get_permissions_for_rendering(rendering_id)).to eq nil
+        end
+
+        it 'should log the not found error' do
+          described_class.get_permissions_for_rendering(rendering_id)
+          expect(FOREST_LOGGER).to have_received(:error).with('Cannot retrieve the permissions from the Forest server.')
+          expect(FOREST_LOGGER).to have_received(:error).with('Which was caused by:')
+          expect(FOREST_LOGGER).to have_received(:error).with(' Forest API returned an HTTP error 404')
+        end
       end
     end
   end
