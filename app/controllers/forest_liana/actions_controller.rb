9081f2ced9f06e4aba7cf13d0d03cf7a8298de6d
GuillaumeCisco <guillaumecisco@gmail.com>
2020-12-01 10:34:18 +0100

    test: add tests

--- app/controllers/forest_liana/actions_controller.rb
+++ app/controllers/forest_liana/actions_controller.rb
@@ -1,54 +1,41 @@
 module ForestLiana
   class ActionsController < ForestLiana::BaseController
 
-    def get_collection(collection_name)
-      ForestLiana.apimap.find { |collection| collection.name.to_s == collection_name }
-    end
-
-    def get_action(collection)
-      collection.actions.find {|action| ActiveSupport::Inflector.parameterize(action.name) == params[:action_name]}
-    end
-
     def values
       render serializer: nil, json: {}, status: :ok
     end
 
+    def get_collection(collection_name)
+      ForestLiana.apimap.find { |collection| collection.name.to_s == collection_name }
+    end
+
+    def get_action(collection_name)
+      collection = get_collection(collection_name)
+      begin
+         collection.actions.find {|action| ActiveSupport::Inflector.parameterize(action.name) == params[:action_name]}
+      rescue => error
+        FOREST_LOGGER.error "Smart Action get action retrieval error: #{error}"
+        nil
+      end
+    end
+
     def get_smart_action_load_ctx(fields)
       fields = fields.reduce({}) {|p, c| p.update(c[:field] => c.merge!(value: nil))}
-      begin
-        {:record => params[:recordIds][0], :fields => fields}
-      rescue => error
-        FOREST_LOGGER.error "Smart Action load context retrieval error: #{error}"
-        {}
-      end
+      {:record => params[:recordIds][0], :fields => fields}
     end
 
     def get_smart_action_change_ctx(fields)
       fields = fields.reduce({}) {|p, c| p.update(c[:field] => c.permit!.to_h)}
-      begin
-        {:record => params[:recordIds][0], :fields => fields}
-      rescue => error
-        FOREST_LOGGER.error "Smart Action change context retrieval error: #{error}"
-        {}
-      end
+      {:record => params[:recordIds][0], :fields => fields}
     end
 
-    def load
-      collection = get_collection(params[:collectionName])
-      action = get_action(collection)
-
-      # Transform fields from array to an object to ease usage in hook, adds null value.
-      context = get_smart_action_load_ctx(action.fields)
-
-      # Call the user-defined load hook.
-      result = action.hooks[:load].(context)
-
+    def handle_result(result, formatted_fields, action)
       if result.nil? || !result.is_a?(Hash)
-        return render status: 500, json: { error: 'Error in smart action load hook: load hook must return an object' }
+        return render status: 500, json: { error: 'Error in smart action load hook: hook must return an object' }
       end
-      is_same_data_structure = ForestLiana::IsSameDataStructureHelper::Analyser.new(context[:fields], result, 1)
+      is_same_data_structure = ForestLiana::IsSameDataStructureHelper::Analyser.new(formatted_fields, result, 1)
       unless is_same_data_structure.perform
-        return render status: 500, json: { error: 'Error in smart action load hook: fields must be unchanged (no addition nor deletion allowed)' }
+        return render status: 500, json: { error: 'Error in smart action hook: fields must be unchanged (no addition nor deletion allowed)' }
       end
 
       # Apply result on fields (transform the object back to an array), preserve order.
@@ -57,29 +44,39 @@ module ForestLiana
       render serializer: nil, json: { fields: fields}, status: :ok
     end
 
+    def load
+      action = get_action(params[:collectionName])
+
+      if !action
+        render status: 500, json: {error: 'Error in smart action load hook: cannot retrieve action from collection'}
+      else
+        # Transform fields from array to an object to ease usage in hook, adds null value.
+        context = get_smart_action_load_ctx(action.fields)
+        formatted_fields = context[:fields].clone # clone for following test on is_same_data_structure
+
+        # Call the user-defined load hook.
+        result = action.hooks[:load].(context)
+
+        handle_result(result, formatted_fields, action)
+      end
+    end
+
     def change
-      collection = get_collection(params[:collectionName])
-      action = get_action(collection)
+      action = get_action(params[:collectionName])
 
-      # Transform fields from array to an object to ease usage in hook.
-      context = get_smart_action_change_ctx(params[:fields])
+      if !action
+        render status: 500, json: {error: 'Error in smart action change hook: cannot retrieve action from collection'}
+      else
+        # Transform fields from array to an object to ease usage in hook.
+        context = get_smart_action_change_ctx(params[:fields])
+        formatted_fields = context[:fields].clone # clone for following test on is_same_data_structure
 
-      # Call the user-defined change hook.
-      field_name = field = params[:fields].select {|field| field[:value] != field[:previousValue] }[0][:field]
-      result = action.hooks[:change][field_name].(context)
+        # Call the user-defined change hook.
+        field_name = params[:fields].select { |field| field[:value] != field[:previousValue] }[0][:field]
+        result = action.hooks[:change][field_name].(context)
 
-      if result.nil? || !result.is_a?(Hash)
-        return render status: 500, json: { error: 'Error in smart action change hook: load hook must return an object' }
+        handle_result(result, formatted_fields, action)
       end
-      is_same_data_structure = ForestLiana::IsSameDataStructureHelper::Analyser.new(context[:fields], result, 1)
-      unless is_same_data_structure.perform
-        return render status: 500, json: { error: 'Error in smart action change hook: fields must be unchanged (no addition nor deletion allowed)' }
-      end
-
-      # Apply result on fields (transform the object back to an array), preserve order.
-      fields = action.fields.map {|f| result[f[:field]]}
-
-      render serializer: nil, json: { fields: fields}, status: :ok
     end
   end
 end
