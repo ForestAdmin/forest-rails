814465845d8c9c6d3766de1b49f7543ad2522342
DrRaider <nsailly@live.fr>
2021-01-29 10:51:34 +0100

    feat(auth): return a specific error during authorization if received an error about 2FA

--- app/services/forest_liana/authorization_getter.rb
+++ app/services/forest_liana/authorization_getter.rb
@@ -4,22 +4,38 @@ module ForestLiana
       begin
         route = "/liana/v2/renderings/#{rendering_id.to_s}/authorization"
         headers = { 'forest-token' => auth_data[:forest_token] }
-        query_parameters = {}
 
         response = ForestLiana::ForestApiRequester
-          .get(route, query: query_parameters, headers: headers)
+          .get(route, query: {}, headers: headers)
 
-        if response.code.to_i == 200
+        if response[:code].to_i == 200
           body = JSON.parse(response.body, :symbolize_names => false)
           user = body['data']['attributes']
           user['id'] = body['data']['id']
           user
         else
-            raise "Cannot authorize the user using this forest account. Forest API returned an #{Errors::HTTPErrorHelper.format(response)}"
+          raise generate_authentication_error response
         end
-      rescue
-        raise ForestLiana::Errors::HTTP401Error
       end
+    end
+
+    private
+    def self.generate_authentication_error(error)
+      case error[:message]
+      when ForestLiana::MESSAGES[:SERVER_TRANSACTION][:SECRET_AND_RENDERINGID_INCONSISTENT]
+        return ForestLiana::Errors::InconsistentSecretAndRenderingError.new()
+      when ForestLiana::MESSAGES[:SERVER_TRANSACTION][:SECRET_NOT_FOUND]
+        return ForestLiana::Errors::SecretNotFoundError.new()
+      else
+      end
+
+      serverError = error[:jse_cause][:response][:body][:errors][0] || nil
+
+      if !serverError.nil? && serverError[:name] == ForestLiana::MESSAGES[:SERVER_TRANSACTION][:names][:TWO_FACTOR_AUTHENTICATION_REQUIRED]
+        return ForestLiana::Errors::TwoFactorAuthenticationRequiredError.new()
+      end
+
+      return StandardError.new(error)
     end
   end
 end
