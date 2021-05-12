2c5f3695e90584122fbd342fb0144148da8c7218
Thenkei <morgan.perre@gmail.com>
2021-05-12 10:20:11 +0200

    fix: charts should no longer throws 403 forbidden when not needed

--- app/services/forest_liana/permissions_checker.rb
+++ app/services/forest_liana/permissions_checker.rb
@@ -177,13 +177,12 @@ module ForestLiana
       return false unless pool_permissions
 
       # NOTICE: equivalent to Object.values in js & removes nil values
-      array_query_request_info = @query_request_info.values.filter_map{ |x| x unless x.nil? }
+      array_permission_infos = @query_request_info.values.filter_map{ |x| x unless x.nil? }
 
-      # NOTICE: pool_permissions contains the @query_request_info
-      #   we use the intersection between statPermission and @query_request_info
+      # NOTICE: Is there any pool_permissions containing the array_permission_infos
       return pool_permissions.any? {
         |statPermission|
-          (array_query_request_info & statPermission.values) == array_query_request_info;
+          (array_permission_infos.all? { |info| statPermission.values.include?(info) });
       }
     end
 
