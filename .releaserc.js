9c2aa7eca9d9e17c1a595b5c15ec3f85095b3d49
Jeff LADIRAY <ladirayjeff@gmail.com>
2021-06-01 10:37:11 +0200

    chore: allow to force a release with force-release scope (#472)

--- .releaserc.js
+++ .releaserc.js
@@ -1,6 +1,15 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer', {
+        'releaseRules': [
+          // This rule allow to force a release by adding "force-release" in scope.
+          // Example: `chore(force-release): support new feature`
+          // Source: https://github.com/semantic-release/commit-analyzer#releaserules
+          { scope: 'force-release', release: 'patch' },
+        ],
+      },
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     [
