d638a6bbf6700ba117d4d76d4c0de3fb335d0d17
Valentin Lamatte <valentinl@forestadmin.com>
2021-06-08 15:11:29 +0200

    feat: handle cache a better way

--- app/services/forest_liana/scope_manager.rb
+++ app/services/forest_liana/scope_manager.rb
@@ -1,6 +1,8 @@
 module ForestLiana
   class ScopeManager
     @@scopes_cache = Hash.new
+    # 5 minutes exipration cache
+    @@scope_cache_expiration_delta = 300
 
     def self.get_scope_for_user(user, collection_name)
       raise 'Missing required rendering_id' unless user['rendering_id']
@@ -12,40 +14,41 @@ module ForestLiana
     end
 
     def self.get_collection_scope(rendering_id, collection_name)
-      # TODO: handle unset yet rendering scopes
+      refresh_scopes_cache(rendering_id) if has_cache_expired?(rendering_id)
 
-      refresh_scopes_cache(rendering_id)
       @@scopes_cache[rendering_id][:scopes][collection_name]
     end
 
+    def self.has_cache_expired?(rendering_id)
+      rendering_scopes = @@scopes_cache[rendering_id]
+      return true unless rendering_scopes
+
+      second_since_last_fetch = Time.now - rendering_scopes[:fetched_at]
+      second_since_last_fetch >= @@scope_cache_expiration_delta
+    end
 
     def self.refresh_scopes_cache(rendering_id)
-      if @@scopes_cache.dig(rendering_id, :fetched_at)
-        p 'THERE IS A FETCHED AT'
-        @@scopes_cache[rendering_id][:scopes] = fetch_scopes(rendering_id)
-      else
-        p 'THERE IS NO FETCHED AT'
-        scopes = fetch_scopes(rendering_id)
-        @@scopes_cache[rendering_id] = {
-          :fetched_at => Time.now,
-          :scopes => scopes
-        }
-      end
-
-      p 'SCOPES', @@scopes_cache
+      # TODO: if already existing trigger refresh without waiting for it (Threads ?)
+      scopes = fetch_scopes(rendering_id)
+      @@scopes_cache[rendering_id] = {
+        :fetched_at => Time.now,
+        :scopes => scopes
+      }
     end
 
     def self.fetch_scopes(rendering_id)
       query_parameters = { 'renderingId' => rendering_id }
       response = ForestLiana::ForestApiRequester.get('/liana/scopes', query: query_parameters)
 
-      p response
-
       if response.is_a?(Net::HTTPOK)
         JSON.parse(response.body)
       else
         raise 'Unable to fetch scopes'
       end
+    end
+
+    def self.invalidate_scope_cache(rendering_id)
+      @@scopes_cache.delete(rendering_id)
     end
   end
 end
--- spec/services/forest_liana/scope_manager_spec.rb
+++ spec/services/forest_liana/scope_manager_spec.rb
@@ -4,13 +4,21 @@ module ForestLiana
       let(:rendering_id) { 13 }
       let(:user) { { 'rendering_id' => rendering_id } }
       let(:collection_name) { 'Users' }
-      let(:collection_scope) { 'mon_super_scope' }
-      let(:json_scopes) { JSON.generate({ collection_name => collection_scope }) }
-      let(:scopes_api_call_response) { Net::HTTPOK.new({}, 200, json_scopes) }
+      let(:first_collection_scope) { 'mon_super_scope' }
+      let(:second_collection_scope) { 'mon_super_scope' }
+      let(:first_json_scopes) { JSON.generate({ collection_name => first_collection_scope }) }
+      let(:second_json_scopes) { JSON.generate({ collection_name => second_collection_scope }) }
+      let(:first_scopes_api_call_response) { Net::HTTPOK.new({}, 200, first_json_scopes) }
+      let(:second_scopes_api_call_response) { Net::HTTPOK.new({}, 200, second_json_scopes) }
+
+      before(:each) do
+        described_class.invalidate_scope_cache(rendering_id)
+      end
 
       before do
-        allow(ForestLiana::ForestApiRequester).to receive(:get).and_return(scopes_api_call_response)
-        allow(scopes_api_call_response).to receive(:body).and_return(json_scopes)
+        allow(ForestLiana::ForestApiRequester).to receive(:get).and_return(first_scopes_api_call_response, second_scopes_api_call_response)
+        allow(first_scopes_api_call_response).to receive(:body).and_return(first_json_scopes)
+        allow(second_scopes_api_call_response).to receive(:body).and_return(second_json_scopes)
       end
 
       describe 'with invalid inputs' do
@@ -28,7 +36,7 @@ module ForestLiana
       end
 
       describe 'when the backend return an errored response' do
-        let(:scopes_api_call_response) { Net::HTTPNotFound.new({}, 404, nil) }
+        let(:first_scopes_api_call_response) { Net::HTTPNotFound.new({}, 404, nil) }
 
         it 'should raise an error' do
           expect {
@@ -41,9 +49,32 @@ module ForestLiana
         let(:scope) { described_class.get_scope_for_user(user, collection_name) }
 
         it 'should fetch the relevant scope and return it' do
-          expect(scope).to eq collection_scope
+          expect(scope).to eq first_collection_scope
           expect(ForestLiana::ForestApiRequester).to have_received(:get).once
           expect(ForestLiana::ForestApiRequester).to have_received(:get).with('/liana/scopes', query: { 'renderingId' => rendering_id })
+        end
+      end
+
+      describe 'when retrieving scopes twice before the refresh cache delta' do
+        let(:scope_first_fetch) { described_class.get_scope_for_user(user, collection_name) }
+        let(:scope_second_fetch) { described_class.get_scope_for_user(user, collection_name) }
+
+        it 'should return the same value twice and have fetch the scopes from the backend only once' do
+          expect(scope_first_fetch).to eq first_collection_scope
+          expect(scope_second_fetch).to eq first_collection_scope
+          expect(ForestLiana::ForestApiRequester).to have_received(:get).once.with('/liana/scopes', query: { 'renderingId' => rendering_id })
+        end
+      end
+
+      describe 'when retrieving scopes twice after the refresh cache delta' do
+        let(:scope_first_fetch) { described_class.get_scope_for_user(user, collection_name) }
+        let(:scope_second_fetch) { described_class.get_scope_for_user(user, collection_name) }
+
+        it 'should return the a new value and have fetch the scopes from the backend twice' do
+          expect(scope_first_fetch).to eq first_collection_scope
+          allow(Time).to receive(:now).and_return(Time.now + 20.minutes)
+          expect(scope_second_fetch).to eq second_collection_scope
+          expect(ForestLiana::ForestApiRequester).to have_received(:get).twice.with('/liana/scopes', query: { 'renderingId' => rendering_id })
         end
       end
     end
