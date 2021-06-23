6aa0a43dde583207d8ec6ac626ba2a2feb13ac59
Valentin Lamatte <valentinl@forestadmin.com>
2021-06-23 11:03:41 +0200

    feat: fix tests 2

--- test/services/forest_liana/pie_stat_getter_test.rb
+++ test/services/forest_liana/pie_stat_getter_test.rb
@@ -3,30 +3,43 @@ module ForestLiana
     rendering_id = 13
     user = { 'rendering_id' => rendering_id }
 
-    test 'Pie stat getter with an aggregate on a boolean field' do
-      stat = PieStatGetter.new(BooleanField, {
-        type: "Pie",
-        collection: "boolean_field",
-        timezone: "Europe/Paris",
-        aggregate: "Count",
-        group_by_field: "field"
-      }, user)
+    describe 'with empty scopes' do
+      # Mock empty scopes
+      before do
+        ForestLiana::ScopeManager.invalidate_scope_cache(rendering_id)
+        WebMock.stub_request(:get, "https://api.forestadmin.com/liana/scopes?renderingId=#{rendering_id}")
+          .to_return(status: 200, body: '{}', headers: {})
+      end
 
-      stat.perform
-      assert stat.record.value.count == 0
-    end
+      describe 'with an aggregate on a boolean field' do
+        it 'should count 0 occurences' do
+          stat = PieStatGetter.new(BooleanField, {
+            type: "Pie",
+            collection: "boolean_field",
+            timezone: "Europe/Paris",
+            aggregate: "Count",
+            group_by_field: "field"
+          }, user)
 
-    test 'Pie stat getter with an aggregate on a foreign key' do
-      stat = PieStatGetter.new(BelongsToField, {
-        type: "Pie",
-        collection: "belongs_to_field",
-        timezone: "Europe/Paris",
-        aggregate: "Count",
-        group_by_field: "has_one_field_id"
-      }, user)
+          stat.perform
+          assert stat.record.value.count == 0
+        end
+      end
 
-      stat.perform
-      assert stat.record.value.count == 30
+      describe 'with an aggregate on a foreign key' do
+        it 'should count 30 occurences' do
+          stat = PieStatGetter.new(BelongsToField, {
+            type: "Pie",
+            collection: "belongs_to_field",
+            timezone: "Europe/Paris",
+            aggregate: "Count",
+            group_by_field: "has_one_field_id"
+          }, user)
+
+          stat.perform
+          assert stat.record.value.count == 30
+        end
+      end
     end
   end
 end
--- test/services/forest_liana/value_stat_getter_test.rb
+++ test/services/forest_liana/value_stat_getter_test.rb
@@ -3,72 +3,89 @@ module ForestLiana
     rendering_id = 13
     user = { 'rendering_id' => rendering_id }
 
-    test 'Value stat getter with a simple filter' do
-      stat = ValueStatGetter.new(BooleanField, {
-        type: "Value",
-        collection: "boolean_field",
-        timezone: "Europe/Paris",
-        aggregate: "Count",
-        filters: {
-          field: "field",
-          operator: 'equal',
-          value: "true"
-        }.to_json
-      }, user)
+    describe 'with empty scopes' do
+      # Mock empty scopes
+      before do
+        ForestLiana::ScopeManager.invalidate_scope_cache(rendering_id)
+        WebMock.stub_request(:get, "https://api.forestadmin.com/liana/scopes?renderingId=#{rendering_id}")
+          .to_return(status: 200, body: '{}', headers: {})
+      end
 
-      stat.perform
-      assert stat.record.value[:countCurrent] == 0
-    end
+      describe 'with a simple filter' do
+        it 'should have a countCurrent of 0' do
+          stat = ValueStatGetter.new(BooleanField, {
+            type: "Value",
+            collection: "boolean_field",
+            timezone: "Europe/Paris",
+            aggregate: "Count",
+            filters: {
+              field: "field",
+              operator: 'equal',
+              value: "true"
+            }.to_json
+          }, user)
 
-    test 'Value stat getter with a filter on a belongs_to integer field' do
-      stat = ValueStatGetter.new(BelongsToField, {
-        type: "Value",
-        collection: "belongs_to_field",
-        timezone: "Europe/Paris",
-        aggregate: "Count",
-        filters: {
-          field: "has_one_field:id",
-          operator: 'equal',
-          value: 3
-        }.to_json
-      }, user)
+          stat.perform
+          assert stat.record.value[:countCurrent] == 0
+        end
+      end
 
-      stat.perform
-      assert stat.record.value[:countCurrent] == 1
-    end
+      describe 'with a filter on a belongs_to integer field' do
+        it 'should have a countCurrent of 1' do
+          stat = ValueStatGetter.new(BelongsToField, {
+            type: "Value",
+            collection: "belongs_to_field",
+            timezone: "Europe/Paris",
+            aggregate: "Count",
+            filters: {
+              field: "has_one_field:id",
+              operator: 'equal',
+              value: 3
+            }.to_json
+          }, user)
 
-    test 'Value stat getter with a filter on a belongs_to boolean field' do
-      stat = ValueStatGetter.new(BelongsToField, {
-        type: "Value",
-        collection: "belongs_to_field",
-        timezone: "Europe/Paris",
-        aggregate: "Count",
-        filters: {
-          field: "has_one_field:checked",
-          operator: 'equal',
-          value: "false"
-        }.to_json
-      }, user)
+          stat.perform
+          assert stat.record.value[:countCurrent] == 1
+        end
+      end
 
-      stat.perform
-      assert stat.record.value[:countCurrent] == 0
-    end
+      describe 'with a filter on a belongs_to boolean field' do
+        it 'should have a countCurrent of 0' do
+          stat = ValueStatGetter.new(BelongsToField, {
+            type: "Value",
+            collection: "belongs_to_field",
+            timezone: "Europe/Paris",
+            aggregate: "Count",
+            filters: {
+              field: "has_one_field:checked",
+              operator: 'equal',
+              value: "false"
+            }.to_json
+          }, user)
 
-    test 'Value stat getter with a filter on a belongs_to enum field' do
-      stat = ValueStatGetter.new(BelongsToField, {
-        type: "Value",
-        collection: "belongs_to_field",
-        timezone: "Europe/Paris",
-        aggregate: "Count",
-        filters: {
-          field: "has_one_field:status",
-          operator: 'equal',
-          value: "pending"
-        }.to_json
-      }, user)
+          stat.perform
+          assert stat.record.value[:countCurrent] == 0
+        end
+      end
 
-      stat.perform
-      assert stat.record.value[:countCurrent] == 1
+      describe 'with a filter on a belongs_to enum field' do
+        it 'should have a countCurrent of 1' do
+          stat = ValueStatGetter.new(BelongsToField, {
+            type: "Value",
+            collection: "belongs_to_field",
+            timezone: "Europe/Paris",
+            aggregate: "Count",
+            filters: {
+              field: "has_one_field:status",
+              operator: 'equal',
+              value: "pending"
+            }.to_json
+          }, user)
+
+          stat.perform
+          assert stat.record.value[:countCurrent] == 1
+        end
+      end
     end
   end
 end
