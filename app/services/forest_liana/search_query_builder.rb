4ad91954285e863f2c5a3fd039466b3489833a14
Matt <matthv@gmail.com>
2022-06-27 14:11:14 +0200

    fix: remove default sort on columns created_at & id

--- app/services/forest_liana/search_query_builder.rb
+++ app/services/forest_liana/search_query_builder.rb
@@ -176,10 +176,6 @@ module ForestLiana
             column = field
           end
         end
-      elsif @resource.column_names.include?('created_at')
-        column = ForestLiana::AdapterHelper.format_column_name(@resource.table_name, 'created_at')
-      elsif @resource.column_names.include?('id')
-        column = ForestLiana::AdapterHelper.format_column_name(@resource.table_name, 'id')
       end
 
       if column
