f1a6f6b062053d7f3069a1cd3adf6a399f5620b1
Matt <matthv@gmail.com>
2023-02-14 17:46:00 +0100

    chore: add test forbidden on statscontroller

--- spec/requests/stats_spec.rb
+++ spec/requests/stats_spec.rb
@@ -99,31 +99,38 @@ describe "Stats", type: :request do
       expect(response.status).to eq(200)
     end
 
-    # it 'should respond 401 with no headers' do
-    #   post '/forest/stats/Product', params: JSON.dump(params)
-    #   expect(response.status).to eq(401)
-    # end
-    #
-    # it 'should respond 404 with non existing collection' do
-    #   post '/forest/stats/NoCollection', params: {}, headers: headers
-    #
-    #   expect(response.status).to eq(404)
-    # end
+    it 'should respond 403 Forbidden' do
+      no_admin_token = JWT.encode({
+                                    id: 1,
+                                    email: 'michael.kelso@that70.show',
+                                    first_name: 'Michael',
+                                    last_name: 'Kelso',
+                                    team: 'Operations',
+                                    rendering_id: '1',
+                                    exp: Time.now.to_i + 2.weeks.to_i,
+                                    permission_level: 'user'
+                                  }, ForestLiana.auth_secret, 'HS256')
 
-    # it 'should respond 403 Forbidden' do
-    #   params[:aggregateFieldName] = 'name'
-    #   Rails.cache.delete('forest.stats')
-    #   # allow_any_instance_of(ForestLiana::StatsController).to receive(:find_resource).and_return(Product)
-    #   # data = ForestLiana::Model::Stat.new(value: { countCurrent: 0, countPrevious: 0 })
-    #   # allow_any_instance_of(ForestLiana::ValueStatGetter).to receive(:record) { data }
-    #   # NOTICE: bypass : find_resource error
-    #   # allow_any_instance_of(ForestLiana::StatsController).to receive(:find_resource) { true }
-    #   # allow(ForestLiana::QueryHelper).to receive(:get_one_association_names_symbol) { true }
-    #
-    #   post '/forest/stats/Product', params: JSON.dump(params), headers: headers
-    #
-    #   expect(response.status).to eq(403)
-    # end
+      no_admin_headers = {
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+        'Authorization' => "Bearer #{no_admin_token}"
+      }
+
+      params[:aggregateFieldName] = 'foo'
+      Rails.cache.delete('forest.stats')
+      allow_any_instance_of(ForestLiana::StatsController).to receive(:find_resource).and_return(Product)
+      data = ForestLiana::Model::Stat.new(value: { countCurrent: 0, countPrevious: 0 })
+      allow_any_instance_of(ForestLiana::ValueStatGetter).to receive(:record) { data }
+      # NOTICE: bypass : find_resource error
+      allow_any_instance_of(ForestLiana::StatsController).to receive(:find_resource) { true }
+      allow(ForestLiana::QueryHelper).to receive(:get_one_association_names_symbol) { true }
+
+
+      post '/forest/stats/Products', params: JSON.dump(params), headers: no_admin_headers
+
+      expect(response.status).to eq(403)
+    end
   end
 
   describe 'POST /stats' do
@@ -143,22 +150,29 @@ describe "Stats", type: :request do
       expect(response.status).to eq(401)
     end
 
-    # it 'should respond 403 Forbidden' do
-    #   params[:query] = 'SELECT COUNT(*) AS value FROM trees;'
-    #   Rails.cache.delete('forest.stats')
-    #   allow_any_instance_of(ForestLiana::StatsController).to receive(:find_resource).and_return(Product)
-    #
-    #   post '/forest/stats', params: JSON.dump(params), headers: headers
-    #   expect(response.status).to eq(403)
-    # end
+    it 'should respond 403 Forbidden' do
+      no_admin_token = JWT.encode({
+                                    id: 1,
+                                    email: 'michael.kelso@that70.show',
+                                    first_name: 'Michael',
+                                    last_name: 'Kelso',
+                                    team: 'Operations',
+                                    rendering_id: '16',
+                                    exp: Time.now.to_i + 2.weeks.to_i,
+                                    permission_level: 'user'
+                                  }, ForestLiana.auth_secret, 'HS256')
 
-    # it 'should respond 422 with unprocessable query' do
-    #   allow_any_instance_of(ForestLiana::StatsController).to receive(:find_resource).and_return(Product)
-    #   allow_any_instance_of(ForestLiana::QueryStatGetter).to receive(:perform) { raise ForestLiana::Errors::LiveQueryError.new }
-    #
-    #   post '/forest/stats', params: JSON.dump(params), headers: headers
-    #   expect(response.status).to eq(422)
-    # end
+      no_admin_headers = {
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+        'Authorization' => "Bearer #{no_admin_token}"
+      }
+      params[:query] = 'SELECT COUNT(*) AS value FROM trees;'
+      Rails.cache.delete('forest.stats')
+      allow_any_instance_of(ForestLiana::StatsController).to receive(:find_resource).and_return(Product)
+
+      post '/forest/stats', params: JSON.dump(params), headers: no_admin_headers
+      expect(response.status).to eq(403)
+    end
   end
-
 end
