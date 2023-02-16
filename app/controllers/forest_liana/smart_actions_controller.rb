882d6dad4b6a35963bf14127784e6122478d3def
Matt <matthv@gmail.com>
2023-02-16 16:37:38 +0100

    refactor: smart-action-controller

--- app/controllers/forest_liana/smart_actions_controller.rb
+++ app/controllers/forest_liana/smart_actions_controller.rb
@@ -10,15 +10,6 @@ module ForestLiana
       before_action :smart_action_pre_perform_checks
     end
 
-    private
-
-    def smart_action_pre_perform_checks
-      get_smart_action_request
-      find_resource
-      check_permission_for_smart_route
-      ensure_record_ids_in_scope
-    end
-
     def access_denied(exception)
       errors = {
         status: exception.error_code,
@@ -31,9 +22,19 @@ module ForestLiana
       render json: { errors: [errors] }, status: exception.status
     end
 
+    private
+
+    def smart_action_pre_perform_checks
+      get_smart_action_request
+      find_resource
+      check_permission_for_smart_route
+      ensure_record_ids_in_scope
+    end
+
     def get_smart_action_request
       begin
         params[:data][:attributes]
+        @parameters = ForestLiana::Ability::Permission::RequestPermission::decodeSignedApprovalRequest(params.permit!)
       rescue => error
         FOREST_REPORTER.report error
         FOREST_LOGGER.error "Smart Action execution error: #{error}"
@@ -41,9 +42,39 @@ module ForestLiana
       end
     end
 
+    def find_resource
+      begin
+        @resource = SchemaUtils.find_model_from_collection_name(@parameters[:data][:attributes][:collection_name])
+        if @resource.nil? || !SchemaUtils.model_included?(@resource) ||
+          !@resource.ancestors.include?(ActiveRecord::Base)
+          render serializer: nil, json: { status: 404 }, status: :not_found
+        end
+        @resource
+      rescue => error
+        FOREST_REPORTER.report error
+        FOREST_LOGGER.error "Find Collection error: #{error}\n#{format_stacktrace(error)}"
+        render serializer: nil, json: { status: 404 }, status: :not_found
+      end
+    end
+
+    def check_permission_for_smart_route
+      smart_action_request = @parameters[:data][:attributes]
+      if !smart_action_request.nil? && smart_action_request.has_key?(:smart_action_id)
+        forest_authorize!(
+          'action',
+          forest_user,
+          @resource,
+          {parameters: params, endpoint: request.fullpath.split('?').first, http_method: request.request_method}
+        )
+      else
+        FOREST_LOGGER.error 'Smart action execution error: Unable to retrieve the smart action id.'
+        render serializer: nil, json: { status: 400 }, status: :bad_request
+      end
+    end
+
     def ensure_record_ids_in_scope
       begin
-        attributes = params[:data][:attributes]
+        attributes = @parameters[:data][:attributes]
 
         # if performing a `selectAll` let the `get_ids_from_request` handle the scopes
         return if attributes[:all_records]
@@ -67,37 +98,6 @@ module ForestLiana
         FOREST_REPORTER.report error
         FOREST_LOGGER.error "Smart Action: #{error}\n#{format_stacktrace(error)}"
         render serializer: nil, json: { error: 'Smart Action: failed to evaluate permissions' }, status: :internal_server_error
-      end
-    end
-
-    def check_permission_for_smart_route
-      smart_action_request = params[:data][:attributes]
-      if !smart_action_request.nil? && smart_action_request.has_key?(:smart_action_id)
-        forest_authorize!(
-          'action',
-          forest_user,
-          @resource,
-          {parameters: params.permit!, endpoint: request.fullpath.split('?').first, http_method: request.request_method}
-        )
-      else
-        FOREST_LOGGER.error 'Smart action execution error: Unable to retrieve the smart action id.'
-        render serializer: nil, json: { status: 400 }, status: :bad_request
-      end
-    end
-
-    def find_resource
-      begin
-        @resource = SchemaUtils.find_model_from_collection_name(params[:data][:attributes][:collection_name])
-
-        if @resource.nil? || !SchemaUtils.model_included?(@resource) ||
-          !@resource.ancestors.include?(ActiveRecord::Base)
-          render serializer: nil, json: { status: 404 }, status: :not_found
-        end
-        @resource
-      rescue => error
-        FOREST_REPORTER.report error
-        FOREST_LOGGER.error "Find Collection error: #{error}\n#{format_stacktrace(error)}"
-        render serializer: nil, json: { status: 404 }, status: :not_found
       end
     end
   end
