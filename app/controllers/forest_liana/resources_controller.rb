da1f8b6cf22d1701a83c40e2a79d056622566715
Matthieu <matthv@gmail.com>
2023-10-05 14:06:43 +0200

    fix: destroy record with restriction on children (#632)
    close #630

--- app/controllers/forest_liana/resources_controller.rb
+++ app/controllers/forest_liana/resources_controller.rb
@@ -129,7 +129,7 @@ module ForestLiana
 
     def destroy
       forest_authorize!('delete', forest_user, @resource)
-        begin
+      begin
         collection_name = ForestLiana.name_for(@resource)
         scoped_records = ForestLiana::ScopeManager.apply_scopes_on_records(@resource, forest_user, collection_name, params[:timezone])
 
@@ -137,9 +137,12 @@ module ForestLiana
           return render serializer: nil, json: { status: 404 }, status: :not_found
         end
 
-        scoped_records.destroy(params[:id])
-
-        head :no_content
+        if scoped_records.destroy(params[:id])
+          head :no_content
+        else
+          restrict_error = ActiveRecord::DeleteRestrictionError.new
+          render json: { errors: [{ status: :bad_request, detail: restrict_error.message }] }, status: :bad_request
+        end
       rescue => error
         FOREST_REPORTER.report error
         FOREST_LOGGER.error "Record Destroy error: #{error}\n#{format_stacktrace(error)}"
@@ -151,9 +154,14 @@ module ForestLiana
       forest_authorize!('delete', forest_user, @resource)
       begin
         ids = ForestLiana::ResourcesGetter.get_ids_from_request(params, forest_user)
-        @resource.destroy(ids) if ids&.any?
-
-        head :no_content
+        @resource.transaction do
+          ids.each do |id|
+            record = @resource.find(id)
+            record.destroy!
+          end
+        end
+      rescue ActiveRecord::RecordNotDestroyed => error
+        render json: { errors: [{ status: :bad_request, detail: error.message }] }, status: :bad_request
       rescue => error
         FOREST_REPORTER.report error
         FOREST_LOGGER.error "Records Destroy error: #{error}\n#{format_stacktrace(error)}"
