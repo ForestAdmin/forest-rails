f7fde38a13002dab7d89ec838cd5430fe22bca11
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2023-12-04 12:20:24 +0100

    chore: update CI config

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['main', {name: 'beta', prerelease: true}],
+  branches: ['main', '+([0-9])?(.{+([0-9]),x}).x', {name: 'beta', prerelease: true}],
   plugins: [
     [
       '@semantic-release/commit-analyzer', {
