bfdbe87d8393f123c98bce9c2bdccafb677b4dcb
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2023-12-18 14:21:44 +0100

    test: remove useless tests

--- spec/lib/forest_liana/schema_file_updater_spec.rb
+++ spec/lib/forest_liana/schema_file_updater_spec.rb
@@ -43,49 +43,6 @@ module ForestLiana
             expect(schema_file_updater.instance_variable_get(:@collections))
               .to eq(collections)
           end
-
-          describe "when a smart action field is malformed" do
-            it "should display a warning message" do
-              collections = [{
-                "fields" => [],
-                "actions" => [{
-                  "fields" => [{}],
-                  "name" => "test",
-                  "hooks" => {
-                    "change" => []
-                  }
-                }],
-                "segments" => []
-              }]
-              allow(FOREST_LOGGER).to receive(:warn)
-              schema_file_updater = ForestLiana::SchemaFileUpdater.new("test.txt", collections, {})
-              expect(FOREST_LOGGER).to have_received(:warn).with('Error while parsing action "test": The field attribute must be defined')
-            end
-          end
-
-          describe "when a smart action change field hook does not exist" do
-            it "should display an error message" do
-              collections = [{
-                "fields" => [],
-                "actions" => [{
-                  "fields" => [{
-                    "field" => "testField",
-                    "hook" => "undefinedHook",
-                    "type" => "String",
-                  }],
-                  "name" => "test",
-                  "hooks" => {
-                    "change" => []
-                  }
-                }],
-                "segments" => []
-              }]
-
-              allow(FOREST_LOGGER).to receive(:error)
-              schema_file_updater = ForestLiana::SchemaFileUpdater.new("test.txt", collections, {})
-              expect(FOREST_LOGGER).to have_received(:error).with('The hook "undefinedHook" of "testField" field on the smart action "test" is not defined.')
-            end
-          end
         end
 
         describe "when the collection has a smart action action" do
