46733df2465d55e6b7c532abcd48197b438397c3
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2024-01-25 15:11:16 +0100

    fix(search): allow search on uuid v7 and v8

--- app/services/forest_liana/search_query_builder.rb
+++ app/services/forest_liana/search_query_builder.rb
@@ -1,6 +1,6 @@
 module ForestLiana
   class SearchQueryBuilder
-    REGEX_UUID = /\A[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/i
+    REGEX_UUID = /\A[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/i
 
     attr_reader :fields_searched
 
