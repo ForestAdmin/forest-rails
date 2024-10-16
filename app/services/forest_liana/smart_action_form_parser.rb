fbdc71be4ceaa31f4226d78cc349eb0703593d8a
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2024-10-16 17:48:17 +0200

    feat: add validation on layout element

--- app/services/forest_liana/smart_action_form_parser.rb
+++ app/services/forest_liana/smart_action_form_parser.rb
@@ -5,14 +5,13 @@ module ForestLiana
       layout = []
       form&.each do |element|
         if element[:type] == 'Layout'
-          # TODO: validate layout element
+          validate_layout_element(element)
+          element[:component] = element[:component].camelize(:lower)
           if %w[Page Row].include?(element[:component])
             extract = extract_fields_and_layout_for_component(element)
-            element[:component] = element[:component].camelize(:lower)
             layout << element
             fields.concat(extract[:fields])
           else
-            element[:component] = element[:component].camelize(:lower)
             layout << element
           end
         else
@@ -26,11 +25,45 @@ module ForestLiana
     end
 
     def self.extract_fields_and_layout_for_component(element)
-      key = element[:component] == 'Page' ? :elements : :fields
+      # 'page' is in camel case because at this step the 'component' attribute is already convert for the response
+      key = element[:component] == 'page' ? :elements : :fields
       extract = extract_fields_and_layout(element[key])
       element[key] = extract[:layout]
 
       extract
+    end
+
+    def self.validate_layout_element(element)
+      valid_components = %w[Page Row Separator HtmlBlock]
+      unless valid_components.include?(element[:component])
+        raise ForestLiana::Errors::HTTP422Error.new(
+          "#{element[:component]} is not a valid component. Valid components are #{valid_components.join(' or ')}"
+        )
+      end
+
+      if element[:component] == 'Page'
+        unless element[:elements].is_a? Array
+          raise ForestLiana::Errors::HTTP422Error.new(
+            "Page components must contain an array of fields or layout elements in property 'elements'"
+          )
+        end
+
+        if element[:elements].any? { |element| element[:component] === 'Page' }
+          raise ForestLiana::Errors::HTTP422Error.new('Pages cannot contain other pages')
+        end
+      end
+
+      if element[:component] == 'Row'
+        unless element[:fields].is_a? Array
+          raise ForestLiana::Errors::HTTP422Error.new(
+            " throw new Error('Row components must contain an array of fields in property 'fields'"
+          )
+        end
+
+        if element[:elements].any? { |element| element[:type] === 'Layout' }
+          raise ForestLiana::Errors::HTTP422Error.new('Row components can only contain fields')
+        end
+      end
     end
   end
 end
