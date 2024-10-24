03b3b632beb564ad332981a80dd36e260a224af3
Matthieu <matthv@gmail.com>
2024-10-24 11:41:20 +0200

    fix: ensure correct values are returned for user tags in context variables (#692)

--- spec/services/forest_liana/utils/context_variables_spec.rb
+++ spec/services/forest_liana/utils/context_variables_spec.rb
@@ -35,8 +35,23 @@ module ForestLiana
       it 'returns the corresponding value from the key provided of the user data' do
         context_variables = described_class.new(team, user, request_context_variables)
         expect(context_variables.get_value('currentUser.first_name')).to eq('John')
-        expect(context_variables.get_value('currentUser.tags.key')).to eq('tag1')
+        expect(context_variables.get_value('currentUser.tags.tag1')).to eq('value1')
         expect(context_variables.get_value('currentUser.team.id')).to eq(1)
+      end
+
+      it 'returns nil when the user key does not exist' do
+        context_variables = described_class.new(team, user, request_context_variables)
+        expect(context_variables.get_value('currentUser.non_existent_key')).to eq(nil)
+      end
+
+      it 'returns nil when the tag key does not exist' do
+        context_variables = described_class.new(team, user, request_context_variables)
+        expect(context_variables.get_value('currentUser.tags.non_existent_tag')).to eq(nil)
+      end
+
+      it 'returns nil when the request context variable key does not exist' do
+        context_variables = described_class.new(team, user, request_context_variables)
+        expect(context_variables.get_value('non_existent_key')).to eq(nil)
       end
     end
   end
