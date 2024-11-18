73d9874f925e2868c996dcae02f1e4070b3d06ed
Matthieu <matthv@gmail.com>
2024-11-18 11:44:10 +0100

    fix(permission): get approval conditions by role id  (#698)

--- app/services/forest_liana/ability/permission/smart_action_checker.rb
+++ app/services/forest_liana/ability/permission/smart_action_checker.rb
@@ -22,7 +22,7 @@ module ForestLiana
 
         def can_approve?
           @parameters = RequestPermission.decodeSignedApprovalRequest(@parameters)
-          if ((@smart_action['userApprovalConditions'].empty? || match_conditions('userApprovalConditions')) &&
+          if ((condition_by_role_id(@smart_action['userApprovalConditions']).blank? || match_conditions('userApprovalConditions')) &&
             (@parameters[:data][:attributes][:requester_id] != @user['id'] || @smart_action['selfApprovalEnabled'].include?(@user['roleId']))
           )
             return true
@@ -33,12 +33,12 @@ module ForestLiana
 
         def can_trigger?
           if @smart_action['triggerEnabled'].include?(@user['roleId']) && @smart_action['approvalRequired'].exclude?(@user['roleId'])
-            return true if @smart_action['triggerConditions'].empty? || match_conditions('triggerConditions')
+            return true if condition_by_role_id(@smart_action['triggerConditions']).blank? || match_conditions('triggerConditions')
           elsif @smart_action['approvalRequired'].include?(@user['roleId'])
-            if @smart_action['approvalRequiredConditions'].empty? || match_conditions('approvalRequiredConditions')
+            if condition_by_role_id(@smart_action['approvalRequiredConditions']).blank? || match_conditions('approvalRequiredConditions')
               raise ForestLiana::Ability::Exceptions::RequireApproval.new(@smart_action['userApprovalEnabled'])
             else
-              return true if @smart_action['triggerConditions'].empty? || match_conditions('triggerConditions')
+              return true if condition_by_role_id(@smart_action['triggerConditions']).blank? || match_conditions('triggerConditions')
             end
           end
 
@@ -48,8 +48,10 @@ module ForestLiana
         def match_conditions(condition_name)
           begin
             attributes = @parameters[:data][:attributes]
+            condition = condition_by_role_id(@smart_action[condition_name])
+
             records = FiltersParser.new(
-              @smart_action[condition_name][0]['filter'],
+              condition['filter'],
               @collection,
               @parameters[:timezone],
               @parameters
@@ -66,6 +68,10 @@ module ForestLiana
           rescue
             raise ForestLiana::Ability::Exceptions::ActionConditionError.new
           end
+        end
+
+        def condition_by_role_id(condition)
+          condition.find { |c| c['roleId'] == @user['roleId'] }
         end
       end
     end
