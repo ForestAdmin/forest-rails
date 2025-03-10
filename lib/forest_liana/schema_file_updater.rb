ForestAdmin/forest-rails

commit ffee03897eb25d368e400597e9d6ceaf941bb467

fix(schema-updater): properly write validation regex when writing schema on disk (#720)

lib/forest_liana/schema_file_updater.rb
@@ -149,7 +149,7 @@ module ForestLiana
         file.puts pretty_print({
           collections: @collections,
           meta: @meta
-        }).gsub(/[^\\](\\)[^\\"]/) { |x| x.gsub($1, "\\\\\\") }
+        }).gsub(/\\/, '\\\\\\\\')
       end
     end
   end

commit 234426cc1d86dad8697d6ffaa6e34dacc3f53793

fix(schema-updater): fix regression introduced with #720 regex when writing schema

lib/forest_liana/schema_file_updater.rb
@@ -149,7 +149,7 @@ module ForestLiana
         file.puts pretty_print({
           collections: @collections,
           meta: @meta
-        }).gsub(/\\/, '\\\\\\\\')
+        }).gsub(/(?<!\\)(\\)(?!["\\])/, '\\\\\\1')
       end
     end
   end