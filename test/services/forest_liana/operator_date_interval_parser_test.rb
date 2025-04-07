0ba928257f06bae4f03081d52733d4a04b0b4ccf
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2025-04-07 15:52:25 +0200

    test: fix tests on operator date interval parser

--- test/services/forest_liana/operator_date_interval_parser_test.rb
+++ test/services/forest_liana/operator_date_interval_parser_test.rb
@@ -3,6 +3,22 @@ module ForestLiana
     test 'OPERATOR_AFTER_X_HOURS_AGO and OPERATOR_BEFORE_X_HOURS_AGO should not take timezone into account' do
       # Setting a big timezone (GMT+10) on purpose, the timezone should not be applied on the result date
       operatorDateIntervalParser = OperatorDateIntervalParser.new('Australia/Sydney')
+      fake_field_schema = {
+        field: "foo",
+        type: "Dateonly",
+        is_filterable: true,
+        is_sortable: true,
+        is_read_only: false,
+        is_required: false,
+        is_virtual: false,
+        default_value: nil,
+        integration: nil,
+        reference: nil,
+        inverse_of: nil,
+        relationships: nil,
+        widget: nil,
+        validations: []
+      }
 
       result = operatorDateIntervalParser.get_date_filter(OperatorDateIntervalParser::OPERATOR_AFTER_X_HOURS_AGO, 2)
       hourComputed = result.split('> ')[1].tr('\'', '').to_datetime.hour
