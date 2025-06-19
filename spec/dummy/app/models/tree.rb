881b39e1fbd6b2ea152df848b0602b3751017934
Matt <matthv@gmail.com>
2025-06-19 17:06:16 +0200

    chore: add tests

--- spec/dummy/app/models/tree.rb
+++ spec/dummy/app/models/tree.rb
@@ -1,7 +1,7 @@
 class Tree < ActiveRecord::Base
   belongs_to :owner, class_name: 'User', inverse_of: :trees_owned
   belongs_to :cutter, class_name: 'User', inverse_of: :trees_cut
-  belongs_to :island
+  belongs_to :island, optional: true
   belongs_to :eponymous_island,
     ->(record) { where(name: record.name) },
     class_name: 'Island',
