6fb7a31ef28dcc8d6370e84377cdb30c39e7dfa0
Matt <matthv@gmail.com>
2025-10-15 17:51:59 +0200

    chore: add new utils

---
+++ app/services/forest_liana/utils/composite_primary_key_helper.rb
@@ -0,0 +1,27 @@
+module ForestLiana
+  module Utils
+    module CompositePrimaryKeyHelper
+      def self.find_record(scoped_records, resource, id)
+        primary_key = resource.primary_key
+
+        if primary_key.is_a?(Array)
+          id_values = parse_composite_id(id)
+          conditions = primary_key.zip(id_values).to_h
+          scoped_records.find_by(conditions)
+        else
+          scoped_records.find(id)
+        end
+      end
+
+      def self.parse_composite_id(id)
+        return id if id.is_a?(Array)
+
+        if id.to_s.start_with?('[') && id.to_s.end_with?(']')
+          JSON.parse(id.to_s)
+        else
+          raise ForestLiana::Errors::HTTP422Error.new("Composite primary key ID must be in format [value1,value2], received: #{id}")
+        end
+      end
+    end
+  end
+end
